90eafdf016e46b91dc56ee034f9b6796f87bce91
het99 <gocks0922@gmail.com>
2019-08-13 16:59:09 +0900

    Create SampleNibView
    - NibLoadableView를 테스트하기 위한 샘플 뷰

---
+++ Example/SwiftbyHet/SampleNibView.swift
@@ -0,0 +1,16 @@
+//
+//  SampleNibLoadableView.swift
+//  SwiftbyHet_Example
+//
+//  Created by Het Song on 13/08/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import SwiftbyHet
+
+class SampleNibView: NibLoadableView {
+    
+    override func commonInit() {
+        
+    }
+}
